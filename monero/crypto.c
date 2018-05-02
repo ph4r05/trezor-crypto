6e31c6c4c4e969b76ad96b95461fc090d6668a35
Dusan Klinec <dusan.klinec@gmail.com>
2018-05-02 13:18:54 +0200

    xmr/crypto: isnonzero fix

--- monero/crypto.c
+++ monero/crypto.c
@@ -127,10 +127,11 @@ int curve25519_isnegative(const bignum25519 f) {
 int curve25519_isnonzero(const bignum25519 f) {
   unsigned char s[32];
   curve25519_contract(s, f);
-  return (((int) (s[0] | s[1] | s[2] | s[3] | s[4] | s[5] | s[6] | s[7] | s[8] |
+  return ((((int) (s[0] | s[1] | s[2] | s[3] | s[4] | s[5] | s[6] | s[7] | s[8] |
                   s[9] | s[10] | s[11] | s[12] | s[13] | s[14] | s[15] | s[16] | s[17] |
                   s[18] | s[19] | s[20] | s[21] | s[22] | s[23] | s[24] | s[25] | s[26] |
-                  s[27] | s[28] | s[29] | s[30] | s[31]) - 1) >> 8) + 1;
+                  s[27] | s[28] | s[29] | s[30] | s[31]) - 1) >> 8) + 1) & 0x1;
+}
 }
 
 static void curve25519_divpowm1(bignum25519 r, const bignum25519 u, const bignum25519 v) {
