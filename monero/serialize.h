093ffb539f2189a413020defcbc8c69a5dccf691
Dusan Klinec <dusan.klinec@gmail.com>
2018-05-03 14:35:51 +0200

    xmr/serialize: header

--- monero/serialize.h
+++ monero/serialize.h
@@ -2,8 +2,8 @@
 // Created by Dusan Klinec on 02/05/2018.
 //
 
-#ifndef TREZOR_CRYPTO_SERIALIZE_H
-#define TREZOR_CRYPTO_SERIALIZE_H
+#ifndef TREZOR_XMR_SERIALIZE_H
+#define TREZOR_XMR_SERIALIZE_H
 
 #include <stddef.h>
 #include <stdint.h>
@@ -12,4 +12,4 @@ int xmr_size_varint(uint64_t num);
 int xmr_write_varint(uint8_t * buff, size_t buff_size, uint64_t num);
 int xmr_read_varint(uint8_t * buff, size_t buff_size, uint64_t *val);
 
-#endif //TREZOR_CRYPTO_SERIALIZE_H
+#endif //TREZOR_XMR_SERIALIZE_H
